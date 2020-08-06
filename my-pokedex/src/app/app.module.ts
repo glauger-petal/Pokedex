cd668dfb93f0a9c6e83c7051b04388c32eb643f8
Lauger Geoffroy <Jojo@Pauls-MacBook-Pro.local>
2020-08-06 17:39:59 -0400

    project creation

--- my-pokedex/src/app/app.module.ts
+++ my-pokedex/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PokemonsListComponent } from "./pokemons-list/pokemons-list.component";
 import { PokemonDetailComponent } from "./pokemon-detail/pokemon-detail.component";
-import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
 import { BorderCardDirective } from './border-card.directive';
 import { PageNotFoundComponent } from './page-not-found.components';
 
@@ -17,8 +16,7 @@ import { PageNotFoundComponent } from './page-not-found.components';
     PokemonsListComponent,
     PokemonDetailComponent,
     BorderCardDirective,
-    PageNotFoundComponent,
-    FilterPokemonPipePipe],
+    PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
